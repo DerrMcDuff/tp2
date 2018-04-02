bb25689e1242ab9f2ccc3142e985ffa0c5f9466d
Olga Fad <olga.fadeitcheva@gmail.com>
2018-04-02 01:34:59 -0400

    client_thread farci, mais tjrs non fonctionnel snif

--- client/client_thread.c
+++ client/client_thread.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+//#include <time.h>
 
 #include "client_thread.h"
 
@@ -19,7 +20,7 @@ int port_number = -1;
 int num_request_per_client = -1;
 int num_resources = -1;
 int *provisioned_resources = NULL;
-
+int *max_resources = NULL;
 // Variable d'initialisation des threads clients.
 unsigned int count = 0;
 
@@ -40,6 +41,8 @@ unsigned int count_dispatched = 0;
 // Nombre total de requêtes envoyées.
 unsigned int request_sent = 0;
 
+//time_t t;
+//srand((unsigned)time(&t));
 
 // Vous devez modifier cette fonction pour faire l'envoie des requêtes
 // Les ressources demandées par la requête doivent être choisies aléatoirement
@@ -47,29 +50,84 @@ unsigned int request_sent = 0;
 // ou négatives.
 // Assurez-vous que la dernière requête d'un client libère toute les ressources
 // qu'il a jusqu'alors accumulées.
+
+int 
+random_ressources_request (int max_res, int live_res)
+{
+  //on choisit une qte aleatoire de la meme ressource par requete
+  //sans depasser le maximum pour le client
+  //on choisit aleatoire si on demande ou si on libere une ressource
+  int req;
+  int r = rand()%(max_res);
+  int b = rand()%2;
+  if(b==0) req = r;
+  else req = -r;
+
+  return req;
+}
+
+
 void
 send_request (int client_id, int request_id, int socket_fd)
 {
-  // TP2 TODO
-
+  
+  char request[64];
+  strcpy(request, "REQ ");
+  sprintf(request, "%d ", socket_fd);
+  
   fprintf (stdout, "Client %d is sending its %d request\n", client_id,
       request_id);
-  
-  // TP2 TODO:END
 
+  for (int i = 0; i < num_request_per_client ; i++) {
+    for (int j = 0; j < num_resources; j++) {
+      
+      int req;
+      int max_res = *(max_resources+j);
+      int prov_res = *(provisioned_resources+j);
+      
+      //si c'est la derniere requete il faut s'assurer que toutes 
+      //les ressources sont liberees
+      if(i == num_request_per_client) {
+        req = -(*(provisioned_resources+j));
+      }
+      
+      //sinon on alloue (ou libere) un nombre aleatoire de ressources
+      else {
+        req = random_ressources_request(max_res, prov_res);
+      }
+      
+      //rajouter la requete de cette ressource
+      sprintf(request, "%d ", socket_fd);
+    }
+    
+    
+    if(send(socket_fd, request, 64, 0) < 0){
+      perror("Send failed");
+      exit(1);
+    }
+      
+    
+  }
+  
 }
+
 
 int
 ct_socket()
 {
   int client_socket = socket(AF_INET,SOCK_STREAM,0);
+  
+  if (client_socket < 0)
+    perror ("ERROR opening socket");
+    exit(1);
+  
   struct sockaddr_in addr;
   addr.sin_family = AF_INET;
   addr.sin_port = htons(port_number); 
   addr.sin_addr.s_addr = htonl(0x7f000001);
   
   if (connect(client_socket, (struct sockaddr*)&addr, sizeof(addr))<0) {
-    perror("ERROR creating socket :(");
+    perror("ERROR on binding :(");
     exit(1);
   }
   return client_socket;
@@ -80,50 +138,51 @@ send_test(void *param) {
   request_sent = request_sent + 1;
   int socket = ct_socket();
   FILE *socket_w = fdopen (socket, "w");
-  fprintf (socket_w, "BEG 6\n");
-  fflush(socket_w);
-}
-
-void
-send_client_amount(int ressource_nb, int client_nb) {
-  request_sent = request_sent + 1;
-  int socket = ct_socket();
-  FILE *socket_w = fdopen (socket, "w");
-  fprintf (socket_w, "BEG %d %d\n",ressource_nb,client_nb);
+  fprintf (socket_w, "BEG 5\n");
   fflush(socket_w);
 }
 
 void *
 ct_code (void *param)
 {
-  int socket_fd = -1;
+  // Initialisation d'un socket
+  int socket_fd = ct_socket();
   client_thread *ct = (client_thread *) param;
-  ct_socket();
-//  int client_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-  //  client_thread *ct = (client_thread *) param;
+  FILE *socket_w = fdopen (socket_fd, "w");
+  
+  char *request = NULL;
 
-  // TP2 TODO
+  printf("Enter the request:\n");
+  if(fgets(request, 256, stdin) > 0){
+    
+      fprintf(stderr, "BRUH\n");
+    //envoie la requete au serveur
+    fputs(request, socket_w);
+    
+    //initialisation des petits clients ('INI') et de leurs ressources
+    
+    if(strncmp(strtok(request, " "),"INI", 3) == 0){
+      
+        ct->id = * strtok(NULL, " ");
+        
+        while(strtok(NULL, " ") != NULL){
+            //mettre le pointeur de max resources sur la qte max de la 1e ressource
+            //mettre le pointeur des ressources allouees sur la valeur 0
+            //incrementer le pointeur pour les prochaines resources
+            *max_resources = atoi(strtok(NULL, " "));
+            *provisioned_resources = 0;
+            max_resources++;
+            provisioned_resources++;
+        }
+        
+    } else if(strncmp(strtok(NULL, " "),"CLO", 3) == 0){
+      
+    }
   
-  // Connection au server.
-  //  GESTION D'ERREURS 
-//  if (client_socket_fd < 0)
-//    perror ("ERROR opening socket");
-//    exit(1);
-//
-//  struct sockaddr_in addr;
-//  addr.sin_family = AF_INET;
-//  addr.sin_port = htons(port_number); 
-//  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-  
-  
-  // Connect client socket to server.
-//  if(connect(client_socket_fd, &addr, sizeof(addr) < 0))
-//      perror ("ERROR on binding");
-  
-  // Vous devez ici faire l'initialisation des petits clients (`INI`).
-  
-  
-  // TP2 TODO:END
+  }else {
+    perror("Request not valid.");
+    exit(1);
+  }
 
 
   for (unsigned int request_id = 0; request_id < num_request_per_client;
@@ -145,6 +204,8 @@ ct_code (void *param)
      * delay.tv_sec = 0;
      * nanosleep (&delay, NULL); */
   }
+  
+  fflush(socket_w);
 
   return NULL;
 }
@@ -161,9 +222,15 @@ ct_wait_server ()
 {
 
   // TP2 TODO: IMPORTANT code non valide.
-
-
-  sleep (4);
+  
+//  for (int i = 0; i++; i < num_request_per_client) {
+//    if (...){
+//      count_dispatched += 1;
+//    }
+//    else{
+//        sleep (4);
+//    }
+//  }
 
   // TP2 TODO:END
   
